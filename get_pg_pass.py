d96faf1e1ecff45b71d8e21ca63f7d59332ad997
Pamela Fox <pamela.fox@gmail.com>
2023-08-23 23:41:28 +0000

    Reverts

--- get_pg_pass.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from azure.identity import DefaultAzureCredential
-
-
-def get_conn():
-    azure_credential = DefaultAzureCredential()
-    token = azure_credential.get_token("https://ossrdbms-aad.database.windows.net")
-    return token.token
